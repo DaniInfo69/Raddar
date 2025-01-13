4d03a679e20d7afcc3ee6cfe41e03d9599fb3bbd
DaniInfo69 <dajosolmag@gmail.com>
2025-01-13 15:34:14 -0600

    Init project

---
+++ Avisen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Avisen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Avisen/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Avisen
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
