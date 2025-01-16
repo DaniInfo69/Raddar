daa86a206a365ce000d4175929692400e79abae1
DaniInfo69 <dajosolmag@gmail.com>
2025-01-15 21:58:34 -0600

    Carpeta Views y archivo Login fueron añadidos

---
+++ Avisen/Views/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace Avisen.Views;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+}
--- Avisen/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/Avisen.AssemblyInfo.cs
+++ Avisen/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/Avisen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4d03a679e20d7afcc3ee6cfe41e03d9599fb3bbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
