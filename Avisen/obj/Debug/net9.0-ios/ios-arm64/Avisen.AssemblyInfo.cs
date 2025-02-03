7c2f1dc5789d39b186190977e0418d78815f54a8
Jony15DGk <jonathandgk15@gmail.com>
2025-02-03 17:02:26 -0600

    Creación del recuadro de la promoción con imagen e información de la misma

--- Avisen/obj/Debug/net9.0-ios/ios-arm64/Avisen.AssemblyInfo.cs
+++ Avisen/obj/Debug/net9.0-ios/ios-arm64/Avisen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d0fbdcf71e4e23b1d1b0760fe7c7aa7d1f0b88c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+db58dbfb4239df57b3deca5117f46f6cc1fa1a7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
