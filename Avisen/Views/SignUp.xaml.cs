12a9819fa494fcb390b9e94deae6a62595b91484
DaniInfo69 <dajosolmag@gmail.com>
2025-02-25 15:08:27 -0600

    SingUp desing and functionalities was changed

--- Avisen/Views/SignUp.xaml.cs
+++ Avisen/Views/SignUp.xaml.cs
@@ -6,4 +6,20 @@ public partial class SignUp : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Back_Clicked(object sender, EventArgs e)
+    {
+        // Obtener la página actual
+        var currentPage = Navigation.NavigationStack.LastOrDefault();
+
+        // Navegar hacia atrás
+        await Navigation.PopAsync();
+
+        // Eliminar la página actual de la pila de navegación
+        if (currentPage != null)
+        {
+            Navigation.RemovePage(currentPage);
+        }
+    }
+
 }
--- Avisen/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Avisen.AssemblyInfo.cs
+++ Avisen/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Avisen.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4af57edb8afb97d211c58720710a5a423bc63706")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+df1420d0c3e0793f5bbf1bd964e844294385a5dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
