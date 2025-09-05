68fbc0df11ae2b8087995512c9ab44768d2e6444
Jony15DGk <jonathandgk15@gmail.com>
2025-09-05 01:07:11 -0600

    Hot Offers design was improved

--- Avisen/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Avisen.AssemblyInfo.cs
+++ Avisen/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Avisen.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d70aeda497291253ba5af37c54da69f5de7725da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+06731167cb466381567ca3f5faf749bc70782e65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avisen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
